cee9007c3be5d9bca9aabb3840fc8f6df634ff10
Ednei Romão Gomes Dantas <edgomes20@hotmail.com>
2023-12-20 12:57:40 -0300

    Desafio 2

---
+++ carrinho-compras/js/app.js
@@ -0,0 +1,19 @@
+function adicionar (){
+    //recuperar valores: nome do produto, quantidade e valor
+
+    let nomeProduto = document.getElementById('produto');
+    let quantidade = document.getElementById('quantidade');
+
+    alert(nomeProduto.value);
+    alert(quantidade.value);
+
+
+    //calcular o preço, o nosso subtotal
+    //adiconar no carrinho
+    //atualizar o valor total
+}
+
+
+function limpar () {
+
+}
