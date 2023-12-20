517d0b3434cd08df374ad2eff654e89773691dda
Ednei Romão Gomes Dantas <edgomes20@hotmail.com>
2023-12-20 13:20:25 -0300

    Desafio 2 Completo

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,19 +1,46 @@
+let totalGeral ;
+limpar();
+
 function adicionar (){
     //recuperar valores: nome do produto, quantidade e valor
 
-    let nomeProduto = document.getElementById('produto');
-    let quantidade = document.getElementById('quantidade');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
+    let quantidade = document.getElementById('quantidade').value;
 
-    alert(nomeProduto.value);
+    /*
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
-
+    */
 
     //calcular o preço, o nosso subtotal
+    let preco = quantidade.value * valorUnitario;
+
+    /*
+    alert(preco);
+    */
+
     //adiconar no carrinho
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade} x </span> ${nomeProduto} <span class="texto-azul">R$1400</span>
+  </section>`;
+
+
     //atualizar o valor total
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
+
+
 }
 
 
 function limpar () {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$ 0';
 }
